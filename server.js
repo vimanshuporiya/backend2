dc8ffc9428098996831e97bb043f148a99c58741
vimanshu <Vimanshuporiya123@gmail.com>
2024-02-14 02:54:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ const port = process.env.PORT || 5000;
 connectDB(process.env.MONGO_URI);
 
 const app = express();
-app.use(cors());
+app.use(cors( {
+    origin: 'https://proshop-g407.onrender.com'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
